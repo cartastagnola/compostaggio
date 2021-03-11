30ed64c8738830729604f05eb790860883f5edb9
cartastagnola <stagnoladicarta@gmail.com>
2021-03-11 17:55:08 +0100

    add first files waiting anxiously waiting to be recycled

---
+++ cvExpoler/math_slam.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+// eigen
+#include <Eigen/Dense>
+#include <Eigen/SVD>
+
+
+// mat stuffs
+
+struct
+v3
+{
+    Eigen::Vector3d vec;
+};
+
+struct
+m3
+{
+    Eigen::Matrix3d mat;
+};
+
+inline v3
+V3(double x, double y, double z)
+{
+    v3 result;
+    Eigen::Vector3d vec(x,y,z);
+    result.vec = vec;
+    return(result);
+}
+
+double
+Dot(v3 A, v3 B)
+{
+    double result;
+    result = A.vec.transpose() * B.vec;
+    return(result);
+}
+
+follow the make tutorial
+https://cs.colby.edu/maxwell/courses/tutorials/maketutor/
+
